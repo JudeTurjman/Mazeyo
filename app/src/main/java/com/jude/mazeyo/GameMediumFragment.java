cac701889e26a54a708db21b9e093541690f692e
Jude <judeturjman@gmail.com>
2024-02-13 13:04:30 +0200

    I can go to the Game Medium. I have to go bake...

--- app/src/main/java/com/jude/mazeyo/GameMediumFragment.java
+++ app/src/main/java/com/jude/mazeyo/GameMediumFragment.java
@@ -1,27 +1,19 @@
 package com.jude.mazeyo;
 
-import android.animation.LayoutTransition;
 import android.os.Bundle;
 
-import androidx.annotation.LayoutRes;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link GameMediumFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
-
-    CardView cvMedium;
-
+public class GameMediumFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,7 +24,7 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+    public GameMediumFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +34,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment GameMediumFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static GameMediumFragment newInstance(String param1, String param2) {
+        GameMediumFragment fragment = new GameMediumFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,28 +59,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        cvMedium = getView().findViewById(R.id.cvMediumhome);
-
-        cvMedium.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GoToMedium();
-            }
-        });
-
-    }
-
-
-    public void GoToMedium(){
-        FragmentTransaction ft= getActivity().getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.FrameLayoutMain, new GameMediumFragment());
-        ft.commit();
+        return inflater.inflate(R.layout.fragment_game_medium, container, false);
     }
 }
